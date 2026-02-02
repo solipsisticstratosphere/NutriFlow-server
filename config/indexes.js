6781a0986fb8792b1a45f414b5c16ca18dff138d
Yaroslav Klimenko <klimyarik13@gmail.com>
2026-02-02 17:56:33 +0200

    fix

--- config/indexes.js
+++ config/indexes.js
@@ -3,15 +3,11 @@ const Product = require('../models/Product');
 const Meal = require('../models/Meal');
 const DailyLog = require('../models/DailyLog');
 
-/**
- * Створює оптимальні індекси для всіх колекцій
- * Викликається при старті сервера
- */
+
 const createIndexes = async () => {
   try {
     console.log('Creating database indexes...');
 
-    // User indexes
     await User.collection.createIndex({ email: 1 }, { unique: true });
     console.log('✓ User indexes created');
 
